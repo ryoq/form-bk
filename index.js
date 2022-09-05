71e45ba1de3cb615852e0a74e388bc7fdd4060d1
ryoq <r.r.r.r.r.r.y.oh@gmail.com>
2022-09-06 05:40:52 +0900

    second

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.post("/inquire", upload.single("file"), (req, res) => {
   }
 
   // ここからSlackへ転送する処理
-  const SlackBotToken = "C0416LQGAH2"
+  const SlackBotToken = "C0416LQGAH2";
   //`${process.env.SlackBotToken}`;
   const SlackApiUrl = "https://slack.com/api/";
   const SlackChannel = "xoxb-4031641060806-4038266418178-8lAZFyHvugYOt1vgtpQ9o9J0";
